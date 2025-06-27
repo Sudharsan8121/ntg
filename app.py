38a779a5a8997b4d4dd3960169f257a11f4bb64e
Sudharsan8121 <sudharsansd8055@gmail.com>
2025-06-27 11:14:19 +0530

    app.py

--- app.py
+++ app.py
@@ -558,4 +558,5 @@ def history():
     return render_template('history.html', conversions=conversions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
